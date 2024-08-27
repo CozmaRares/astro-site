ed3d72ba644658ab2d720241c3f07d072007236b
Cozma Rares <cozmarares11@gmail.com>
2024-08-27 19:32:33 +0300

    feat(portfolio/2024): add contact section

--- src/components/portfolio/2024/Contact.tsx
+++ src/components/portfolio/2024/Contact.tsx
@@ -1,3 +1,22 @@
+import { Mail } from "lucide-react";
+
 export default function Contact() {
-  return null;
+  return (
+    <section
+      id="contact"
+      className="section max-w-[40rem] space-y-4 pb-32 text-center sm:pb-40"
+    >
+      <h2 className="text-heading font-heading">Contact me</h2>
+      <p>
+        <span>Contact me directly at </span>
+        <a
+          className="inline-flex flex-row items-center gap-0.5 text-lg underline sm:text-xl"
+          href="mailto:cozmarares11@gmail.com"
+        >
+          cozmarares11@gmail.com
+          <Mail />
+        </a>
+      </p>
+    </section>
+  );
 }

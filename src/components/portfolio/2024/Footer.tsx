84acf516cf1988108179200d17c4c8faf08cee83
Cozma Rares <cozmarares11@gmail.com>
2024-08-27 19:48:25 +0300

    fix(portfolio/2024): extract header and footer from main layout, change footer's margin to padding

--- src/components/portfolio/2024/Footer.tsx
+++ src/components/portfolio/2024/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto mb-10 w-3/4 max-w-[30rem] text-center text-gray-500">
+    <footer className="mx-auto pb-10 w-3/4 max-w-[30rem] text-center text-gray-500">
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         Astro, TypeScript, Tailwind CSS, &amp; deployed with Vercel.

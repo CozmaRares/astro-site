ed9783556195c3915488a530234ccadf3cd9824e
Cozma Rares <cozmarares11@gmail.com>
2024-08-27 19:38:33 +0300

    feat(portfolio/2024): add footer

---
+++ src/components/portfolio/2024/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="mx-auto mb-10 w-3/4 max-w-[30rem] text-center text-gray-500">
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> built with
+        Astro, TypeScript, Tailwind CSS, &amp; deployed with Vercel.
+      </p>
+    </footer>
+  );
+}

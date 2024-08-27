e7402db10538a3c8e1ce2181bb11552b72818cf2
Cozma Rares <cozmarares11@gmail.com>
2024-08-27 19:54:26 +0300

    refactor(portfolio/2024): reword footer

--- src/components/portfolio/2024/Footer.tsx
+++ src/components/portfolio/2024/Footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto pb-10 w-3/4 max-w-[30rem] text-center text-gray-500">
+    <footer className="mx-auto w-3/4 max-w-[30rem] pb-10 text-center text-gray-500">
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        Astro, TypeScript, Tailwind CSS, &amp; deployed with Vercel.
+        <p className="pb-2 font-bold">Built With</p>
+        <p>Astro, TypeScript, Tailwind CSS</p>
+        <p className="font-semibold italic">&amp; deployed with Vercel.</p>
       </p>
     </footer>
   );

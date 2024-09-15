d14b5e77d764814c61a9d883716f8a905909eb3f
Cozma Rares <cozmarares11@gmail.com>
2024-09-15 19:57:48 +0300

    feat(portfolio/2024): add theme switch button

--- src/components/portfolio/2024/FeaturedProjects.tsx
+++ src/components/portfolio/2024/FeaturedProjects.tsx
@@ -3,11 +3,11 @@ import FeaturedProject from "./helpers/FeaturedProject";
 
 const FeaturedProjects = () => {
   return (
-    <section
-      id="projects"
-      className="section relative scroll-m-48"
-    >
-      <h2 className="text-heading absolute -top-16 left-1/2 -translate-x-1/2 font-heading">
+    <section className="section relative">
+      <h2
+        id="projects"
+        className="text-heading absolute -top-16 left-1/2 -translate-x-1/2 scroll-mt-32 font-heading"
+      >
         Projects
       </h2>
       <ul className="grid gap-8 px-[2px] pb-2 md:grid-cols-2 md:gap-4 lg:grid-cols-1 lg:gap-14">

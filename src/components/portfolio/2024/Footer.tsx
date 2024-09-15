5552dbc0af9bcc9e9f82c02c10bb900242179ae5
Cozma Rares <cozmarares11@gmail.com>
2024-09-15 20:01:36 +0300

    fix(portfolio/2024): fix footer font size

--- src/components/portfolio/2024/Footer.tsx
+++ src/components/portfolio/2024/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
     <footer className="mx-auto w-3/4 max-w-[30rem] pb-10 text-center text-gray-500">
-      <p className="text-xs">
+      <div className="text-xs">
         <p className="pb-2 font-bold">Built With</p>
         <p>Astro, TypeScript, Tailwind CSS</p>
-        <p className="font-semibold italic">&amp; deployed with Vercel.</p>
-      </p>
+        <p className="font-semibold italic">&amp; deployed on Vercel.</p>
+      </div>
     </footer>
   );
 }
